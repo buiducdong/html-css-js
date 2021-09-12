2324ffd106ff302d06e9ba7e8cb67ad87ae6f64b
dong <dong@example.com>
2021-09-13 00:24:45 +0700

    create card used flexbox

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
+        //toggle
         nav.classList.toggle('nav-active');
+        //animation links
+        navLinks.forEach((elementLink, index) => {
+            
+            if(elementLink.style.animation) {
+                elementLink.style.animation = ''
+            } else {
+                elementLink.style.animation = `
+                navLinkFadeOut 1s ease forwards ${index / 6}s`
+            }
+        });
+
+        //burger animation
+        burger.classList.toggle('toggle')
     });
 }
 
