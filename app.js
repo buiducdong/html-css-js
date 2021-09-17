b66b25dc032973c43e0ccaa37db1222547dcffa2
dong <dong@example.com>
2021-09-17 14:04:30 +0700

    create todoList

--- app.js
+++ app.js
@@ -25,82 +25,101 @@ const navSlide = () => {
 
 navSlide();
 
+// ----------------------to do list app----------------------------
 
+(function() {
+  'use strict';
+  var tasker = {
+    init: function() {
+      this.cacheDom();
+      this.bindEvents();
+      this.evalTasklist();
+    },
+    cacheDom: function() {
+      this.taskInput = document.getElementById("input-task");
+      this.addBtn = document.getElementById("add-task-btn");
+      this.tasklist = document.getElementById("tasks");
+      this.tasklistChildren = this.tasklist.children;
+      this.errorMessage = document.getElementById("error");
+    },
+    bindEvents: function() {
+      this.addBtn.onclick = this.addTask.bind(this);
+      this.taskInput.onkeypress = this.enterKey.bind(this);
+    },
+    evalTasklist: function() {
+      var i, chkBox, delBtn;
+      //BIND CLICK EVENTS TO ELEMENTS
+      for (i = 0; i < this.tasklistChildren.length; i += 1) {
+        //ADD CLICK EVENT TO CHECKBOXES
+        chkBox = this.tasklistChildren[i].getElementsByTagName("input")[0];
+        chkBox.onclick = this.completeTask.bind(this, this.tasklistChildren[i], chkBox);
+        //ADD CLICK EVENT TO DELETE BUTTON
+        delBtn = this.tasklistChildren[i].getElementsByTagName("button")[0];
+        delBtn.onclick = this.delTask.bind(this, i);
+      }
+    },
+    render: function() {
+      var taskLi, taskChkbx, taskVal, taskBtn, taskTrsh;
+      //BUILD HTML
+      taskLi = document.createElement("li");
+      taskLi.setAttribute("class", "task");
+      //CHECKBOX
+      taskChkbx = document.createElement("input");
+      taskChkbx.setAttribute("type", "checkbox");
+      //USER TASK
+      taskVal = document.createTextNode(this.taskInput.value);
+      //DELETE BUTTON
+      taskBtn = document.createElement("button");
+      //TRASH ICON
+      taskTrsh = document.createElement("i");
+      taskTrsh.setAttribute("class", "fa fa-trash");
+      //INSTERT TRASH CAN INTO BUTTON
+      taskBtn.appendChild(taskTrsh);
 
-var fakeApi = "http://localhost:3000/content"
+      //APPEND ELEMENTS TO TASKLI
+      taskLi.appendChild(taskChkbx);
+      taskLi.appendChild(taskVal);
+      taskLi.appendChild(taskBtn);
 
-function start() {
-    getContent((contents) => {
-        renderContent(contents)
-    });
-    handleForm();
-}
-start();
+      //ADD TASK TO TASK LIST
+      this.tasklist.appendChild(taskLi);
 
-function getContent(callback) {
-    fetch(fakeApi)
-        .then((api) => {
-            return api.json()
-        })
-        .then(callback);
-}
+    },
+    completeTask: function(i, chkBox) {
+      if (chkBox.checked) {
+        i.className = "task completed";
+      } else {
+        this.incompleteTask(i);
+      }
+    },
+    incompleteTask: function(i) {
+      i.className = "task";
+    },
+    enterKey: function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        this.addTask();
+      }
+    },
+    addTask: function() {
+      var value = this.taskInput.value;
+      this.errorMessage.style.display = "none";
 
-function createContetn(data) {
-    fetch(fakeApi,{
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then((api) => {
-        return api.json()
-    })
-    .then(getContent((contents) => {
-        renderContent(contents)
-    }))
-}
-
-function renderContent(contents) {
-    const listContent = document.getElementById('listContent')
-    const content = contents.map((content) => {
-        return `<li class = "item-${content.id}">
-        <h4>${content.content} - ${content.date}</h4>
-        <button onclick="deleteContent(${content.id})" style="margin-left: 20px;cursor: pointer; background-color: red; border-radius: 4px; padding: 4px 4px">delete</button>
-        </li>`
-    })
-    listContent.innerHTML = content;
-}
-
-function deleteContent(id) {
-    fetch(fakeApi + '/' + id,{
-        method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-        headers: {
-        'Content-Type': 'application/json'
-        }
-    })
-    .then((api ) => {
-        return api.json()
-    })
-    .then(function() {
-        const item = document.querySelector('.item-' + id)
-        if(item) {
-            item.remove();
-        }
-    })
-}
- 
-function handleForm() {
-    var addBtn = document.querySelector('#add');
-    addBtn.onclick = function() {
-        var content = document.querySelector('input[name="content"]').value;
-        var date = document.querySelector('input[name="date"]').value;
-        
-        var formData = {
-            content: content,
-            date: date
-        }
-
-        createContetn(formData)
+      if (value === "") {
+        this.error();
+      } else {
+        this.render();
+        this.taskInput.value = "";
+        this.evalTasklist();
+      }
+    },
+    delTask: function(i) {
+      this.tasklist.children[i].remove();
+      this.evalTasklist();
+    },
+    error: function() {
+      this.errorMessage.style.display = "block";
     }
-}
+  };
+
+  tasker.init();
+}());
